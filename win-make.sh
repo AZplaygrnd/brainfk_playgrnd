5db75178e004baa8406156499015c8b04ada3811
AZplaygrnd <art.zheleznyakov+github@gmail.com>
2024-03-18 23:09:50 +0100

    Adjust the make file

--- win-make.sh
+++ win-make.sh
@@ -1,2 +1,2 @@
 set -xe
-gcc src\\*.c -I.\\include -o .\\bin\\win-interpreter.exe -std=c99 -Wall -Wextra -pedantic
+gcc src\\*.c src\\lib\\*.c -I.\\include -o .\\bin\\win-interpreter.exe -std=c99 -Wall -Wextra -pedantic
